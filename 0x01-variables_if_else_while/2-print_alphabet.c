dd3e4a5ee49280eb9b6ad4c56ea8b758192c4731
Onyejilugha <iyke9732@gmail.com>
2022-09-09 16:27:35 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,18 @@
-#include <stdlib.h>
+#include <stdio.h>
 
-#include <time.h>
+/**
+* main- Entry point
+* @void: Null value
+ *
+* Description: Print alphabet lowercase
+* Return: Zero value
+ */
 
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
