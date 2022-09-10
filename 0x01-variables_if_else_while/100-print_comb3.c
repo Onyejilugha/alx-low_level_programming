5ed4093556f461203eeb3acc92debc790acc698e
Onyejilugha <iyke9732@gmail.com>
2022-09-10 03:54:29 -0700

    Task 0

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,24 +3,21 @@
 /**
  * main- Entry point
  * @void: Null value
-*
+ *
  * Description: Print numbers from 00 to 99
+ * Return: Zero value
  */
 
 int main(void)
 {
-int n;
+	int n;
 
-for (n = 0; n <= 99; n++)
-{
-putchar((n / 10) + '0');
-putchar((n % 10) + '0');
-if (n != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
-}
+	for (n = 0; n <= 99; n++)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+		if (n != 99)
+		{
+			putchar(',');
+			putchar(' ');
+
