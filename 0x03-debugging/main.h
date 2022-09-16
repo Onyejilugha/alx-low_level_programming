da9a20a593a8e761f9118fa12ad7edbf0cf372d7
Onyejilugha <iyke9732@gmail.com>
2022-09-16 10:38:48 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,5 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 void positive_or_negative(int i);
-#endif
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
