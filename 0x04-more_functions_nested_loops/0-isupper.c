133e3219570760337d46473e7b15dc2667e838bf
Onyejilugha <iyke9732@gmail.com>
2022-09-21 17:16:25 -0700

    task 0 correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,8 @@
 /**
  * _isupper - test char for uppercase characters == TRUE
  * @c: int value to be tested
- *
- * Return: 1 if c is uppercase, 0 otherwise.
+ * Return: 1 if c is uppercase
+ * 0 otherwise.
  */
 int _isupper(int c)
 {
