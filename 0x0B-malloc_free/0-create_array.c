90b6c4414642cd49c19a811796964a92ade53b1e
Onyejilugha <iyke9732@gmail.com>
2022-10-13 16:36:04 -0700

    0-create_array.c is ok

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-
 	unsigned int index;
 
 	if (size == 0)
 		return (NULL);
-
 	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
